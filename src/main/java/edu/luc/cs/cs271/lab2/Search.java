5b5633f6f3324e0520b679191cd21fcfd13846de
linmaliakal <lmaliakal@luc.edu>
2017-09-25 05:40:01 +0000

    finished TODO in Search for binary search through array
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs/cs271/lab2/Search.java
+++ src/main/java/edu/luc/cs/cs271/lab2/Search.java
@@ -24,9 +24,9 @@ public class Search {
   /** Looks for the position of the named team in a list. */
   public static Optional<Integer> findTeamPosition(final List<Team> list, final String key) {
     // TODO complete this method
-    final int size = list.size;
+    final int size = list.size();
     for (int i = 0; i < size; i++) {
-      if (list[i].getName().equals(key)) {
+      if (list.get(i).getName().equals(key)) {
         return Optional.of(i);
       }
     }
@@ -40,7 +40,13 @@ public class Search {
    * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
    */
   public static Optional<Integer> findTeamMinFunding(final Team[] arr, final int minFunding) {
-    // TODO complete this method
+    // DONE complete this method
+    final int size = arr.length;
+    for (int i = 0; i < size; i++) {
+      if (arr[i].getFunding() <= (minFunding)) {
+        return Optional.of(i);
+      }
+    }
     return Optional.empty();
   }
   
@@ -54,7 +60,7 @@ public class Search {
    * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
    */
   public static Optional<Integer> findTeamMinFundingFast(final Team[] arr, final int minFunding) {
-    // TODO complete this method
+    // DONE complete this method
     // Gets the array size
     final int size = arr.length;
     // Initially search the entire array
@@ -63,6 +69,20 @@ public class Search {
     // Keep going as long as there is more than one item to be checked
     // Eliminate the wrong half of the array
     // Return current item only if it meets the condition!
+    // Use ideas explained in class (look at notes)
+    while (low < high) {
+      final int middle = ((high + low)/2);
+      if (arr[middle].getFunding() == minFunding) {
+        return Optional.of(middle); // include where found?
+      }
+      else if (arr[middle].getFunding() > minFunding) {
+        high = middle - 1;
+      }
+      else if (arr[middle].getFunding() < minFunding) {
+        low = middle + 1;
+      }
+    }
+    
     if (low <= high && arr[low].getFunding() >= minFunding) {
       return Optional.of(low);
     } else {
